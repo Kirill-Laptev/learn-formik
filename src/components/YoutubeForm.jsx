6c4764e5401288457093e9c975a4a5869c96d072
Kirill <example@gmail.com>
2021-01-28 16:32:25 +0300

    #6.7 Validation form. Validation function

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,20 +1,47 @@
 import React from 'react'
 import { useFormik } from 'formik'
 
+const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+}
+
+const onSubmit = (values) => {
+    console.log(values)
+}
+
+const validate = (values) => {
+    // values.name, values.email, values.channel
+    // errors.name, errors.email, errors.channel
+    // errors.name = 'The field is required'
+    let errors = {}
+
+    if(!values.name){
+      errors.name = 'Required'
+    }
+
+    if(!values.email){
+      errors.email = 'Required'
+    } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email){ 
+        errors.email = 'Invalid format email'
+    }
+
+    if(!values.channel){
+      errors.channel = 'Required'
+    }
+
+    return errors;
+}
+
+
 const YoutubeForm = () => {
 
     const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            channel: ''
-        },
-        onSubmit: (values) => {
-            console.log(values)
-        } 
+        initialValues,
+        onSubmit,
+        validate
     })
-
-    // console.log(formik.values)
 
     return (
         <div>
