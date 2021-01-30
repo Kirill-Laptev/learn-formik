89b8f734907c6b3f56bd2f214071c92318ac5043
Kirill <example@gmail.com>
2021-01-30 15:31:58 +0300

    #21. Arrays in initialValues={}

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -12,7 +12,8 @@ const initialValues = {
   social: {
       twitter: '',
       facebook: ''
-  }
+  },
+  phoneNumbers: ['', '']
 };
 
 const onSubmit = (values) => {
@@ -28,92 +29,77 @@ const validationSchema = Yup.object({
 
 
 const YoutubeForm = () => {
-
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
       <Form>
         <div className='form-control'>
           <label htmlFor='name'>Name</label>
-          <Field
-            type='text'
-            id='name'
-            name='name'
-          />
+          <Field type='text' id='name' name='name' />
           <ErrorMessage name='name' component={TextError} />
         </div>
-    
+
         <div className='form-control'>
           <label htmlFor='email'>E-mail</label>
-          <Field
-            type='email'
-            id='email'
-            name='email'
-          />
+          <Field type='email' id='email' name='email' />
           <ErrorMessage name='email'>
-              {
-                  (errorMsg) => {
-                    return <div className='error'>{errorMsg}</div>
-                  }
-              }
+            {(errorMsg) => {
+              return <div className='error'>{errorMsg}</div>;
+            }}
           </ErrorMessage>
         </div>
 
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
-          <Field
-            type='text'
-            id='channel'
-            name='channel'
-          />
+          <Field type='text' id='channel' name='channel' />
           <ErrorMessage name='channel' />
         </div>
 
         <div className='form-control'>
           <label htmlFor='comments'>Comments</label>
-          <Field
-            as='textarea'
-            id='comments'
-            name='comments'
-          />
+          <Field as='textarea' id='comments' name='comments' />
         </div>
 
         <div className='form-control'>
           <label htmlFor='adress'>Adress</label>
-            <Field name='adress'>
-            {
-                ({field, form, meta}) => {
-                    // console.log(props)
-                    return (
-                        <div>
-                            <input type='text' id='adress' {...field}/>
-                            {meta.error && meta.touched ? <div>{meta.error}</div> : null}    
-                        </div>
-                    )
-                }
-            }
-            </Field>
+          <Field name='adress'>
+            {({ field, form, meta }) => {
+              // console.log(props)
+              return (
+                <div>
+                  <input type='text' id='adress' {...field} />
+                  {meta.error && meta.touched ? <div>{meta.error}</div> : null}
+                </div>
+              );
+            }}
+          </Field>
         </div>
 
 
-
-        // Social
-
+        // Social (obj)
         <div className='form-control'>
           <label htmlFor='twitter'>Twitter profile</label>
-          <Field
-            type='text'
-            id='twitter'
-            name='social.twitter'
-          />
+          <Field type='text' id='twitter' name='social.twitter' />
         </div>
 
         <div className='form-control'>
           <label htmlFor='facebook'>Facebook profile</label>
-          <Field
-            type='text'
-            id='facebook'
-            name='social.facebook'
-          />
+          <Field type='text' id='facebook' name='social.facebook' />
+        </div>
+
+        // Contacts (arr)
+
+        <div className='form-control'>
+          <label htmlFor='firstPh'>Facebook profile</label>
+          <Field type='text' id='firstPh' name='phoneNumbers[0]' />
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='secondPh'>Facebook profile</label>
+          <Field type='text' id='secondPh' name='phoneNumbers[1]' />
         </div>
 
         <button type='submit'>Submit</button>
